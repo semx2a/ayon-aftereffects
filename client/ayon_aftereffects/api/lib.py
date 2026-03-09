c527854b735158eab5009a9b0e0956589f72a2cc
Semiha Beyazkilic <semiha@8849.io>
2026-03-09 15:58:50 +0100

    fix: typo in lib.py

--- client/ayon_aftereffects/api/lib.py
+++ client/ayon_aftereffects/api/lib.py
@@ -27,7 +27,7 @@ def maintained_selection():
 
 def get_extension_manifest_path():
     return os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),f
+        os.path.dirname(os.path.abspath(__file__)),
         "extension",
         "CSXS",
         "manifest.xml"
